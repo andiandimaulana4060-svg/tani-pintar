904b0825708bd66f3f8d5bee174167ed07fdda6c
andiandimaulana4060-svg <andiandimaulana4060@gmail.com>
2025-11-16 22:13:27 +0800

    Update get_chart_data.php

--- get_chart_data.php
+++ get_chart_data.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+
+// lanjut kode aslimu di bawah sini...
+
 header('Content-Type: application/json');
 require_once 'utils.php';
 
@@ -9,3 +19,4 @@ echo json_encode([
     'status' => 'OK',
     'data'   => $data
 ]);
+
