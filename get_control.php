b18cd0394f7793de4ef6de554907e4c52a8080eb
andiandimaulana4060-svg <andiandimaulana4060@gmail.com>
2025-11-16 22:35:05 +0800

    Update get_control.php

--- get_control.php
+++ get_control.php
@@ -7,18 +7,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
-// lanjut kode aslimu di bawah sini...
-
 header('Content-Type: application/json');
 require_once 'utils.php';
 
 ensure_storage();
-$control = read_json(CONTROL_FILE, ['fan' => 0, 'aerator' => 0, 'updated_at' => null]);
+$control = read_json(CONTROL_FILE, [
+    'fan' => 0,
+    'aerator' => 0,
+    'updated_at' => null
+]);
 
 echo json_encode([
     'status'  => 'OK',
-    'fan'     => (int)$control['fan'],
-    'aerator' => (int)$control['aerator'],
-    'updated_at' => $control['updated_at']
+    'data' => [
+        'fan'     => (int)$control['fan'],
+        'aerator' => (int)$control['aerator'],
+        'updated_at' => $control['updated_at']
+    ]
 ]);
-
